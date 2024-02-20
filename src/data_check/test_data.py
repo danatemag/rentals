f7662271ae2de6abe978b8abf64db790227d5097
Dan Dumitriu <dan.dumitriu@emag.ro>
2024-02-20 14:17:25 +0200

    pipeline

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -60,6 +60,14 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
     assert scipy.stats.entropy(dist1, dist2, base=2) < kl_threshold
 
 
-########################################################
-# Implement here test_row_count and test_price_range   #
-########################################################
+def test_row_count(data: pd.DataFrame):
+    assert 15000 < data.shape[0] < 1000000
+
+
+def test_price_range(data: pd.DataFrame, min_price: float, max_price: float):
+    filtered_df = data[data['price'] > max_price] 
+    assert filtered_df.empty
+
+    filtered_df = data[data['price'] < min_price]
+    assert filtered_df.empty 
+
